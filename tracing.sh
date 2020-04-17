55ced05c4a8428b767ad3f9edbbe5fb858e6dfc9
Wentao Cai <wcai6@cs.rochester.edu>
2020-04-17 00:19:35 -0400

    update workloads, add 1m

--- tracing.sh
+++ tracing.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-mvn -pl kvtracer -am clean package
+# mvn -pl kvtracer -am clean package
 TRACE_DIR="traces/"
 WORKLOAD_DIR="workloads/workload"
 WORKLOADS="a b"
-rm ${TRACE_DIR}"*"
+RECORD_AMOUNTS="100k 1m"
+mkdir ${TRACE_DIR}
 for WL in $WORKLOADS
 do
-    WL_PATH=${WORKLOAD_DIR}${WL}
-    for threads in 1 2 4 6 10 16 20 24 32 40
+    for AMT in $RECORD_AMOUNTS
     do
-        ./bin/ycsb load kvtracer -P $WL_PATH -threads $threads -p "kvtracer.tracefile="${TRACE_DIR}${WL}"-load" -p "kvtracer.totalthread="${threads}
-        ./bin/ycsb run kvtracer -P $WL_PATH -threads $threads -p "kvtracer.tracefile="${TRACE_DIR}${WL}"-run" -p "kvtracer.totalthread="${threads}
+        mkdir ${TRACE_DIR}"/"${AMT}
+        WL_PATH=${WORKLOAD_DIR}${WL}"_"${AMT}
+        for threads in 1 2 4 6 10 16 20 24 32 40
+        do
+            ./bin/ycsb load kvtracer -P $WL_PATH -threads $threads -p "kvtracer.tracefile="${TRACE_DIR}"/"${AMT}"/"${WL}"-load" -p "kvtracer.totalthread="${threads}
+            ./bin/ycsb run kvtracer -P $WL_PATH -threads $threads -p "kvtracer.tracefile="${TRACE_DIR}"/"${AMT}"/"${WL}"-run" -p "kvtracer.totalthread="${threads}
+        done
     done
 done
